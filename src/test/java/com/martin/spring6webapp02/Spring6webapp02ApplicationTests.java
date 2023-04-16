29b482d375ddfc6ae55db2fb870378286c5c72f4
MartinMSalas <ingmartinmsalas@gmail.com>
2023-04-16 00:54:12 -0300

    adding files

---
+++ src/test/java/com/martin/spring6webapp02/Spring6webapp02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martin.spring6webapp02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6webapp02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
